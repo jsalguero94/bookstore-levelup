0bdae36ae85f69b59d21a5f0152cee7520753f04
jsalguero94 <jsalguero94@gmail.com>
2022-06-07 18:42:30 -0500

    Request model refactored and regexp modified

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,14 +1,14 @@
-class Request < ApplicationRecord
+# frozen_string_literal: true
 
+class Request < ApplicationRecord
   belongs_to :user
 
-  scope :by_user, -> (current_user) {
-    where("user_id = #{current_user}") 
-  }
-
-  validates :name_of_book, presence: true
-
-  validates :name_of_book, format:{with: /[a-zA-Z0-9]+/, message: "only letters and numbers"}
-  validates :name_of_book, length: { maximum: 30 }
-
+  validates :name_of_book,
+            presence: true,
+            format:
+            {
+              with: /\A[a-zA-Z0-9]+\z/,
+              message: 'only letters and numbers'
+            },
+            length: { maximum: 30 }
 end
